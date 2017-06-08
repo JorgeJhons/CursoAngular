e2c71da55ebbaf4627fa7e4d1cdf5b5684600c28
Jorge Fernandez <jfernandezcolli@gmail.com>
2017-06-08 13:34:59 -0500

    Se aplican los filtros con el arreglo antes creado

--- app/app.js
+++ app/app.js
@@ -2,5 +2,22 @@ var myNinjaApp= angular.module('myNinjaApp',[]);
 
 myNinjaApp.controller('NinjaController',function($scope){
 	$scope.message="Hello BRIAN";
-	$scope.ninjas=['yoshi','cayin','volt','redFlash'];
+	$scope.ninjas=[
+		{
+			name:"yosh",
+			cinta:"verde"
+		},
+		{
+			name:"kayin",
+			cinta:"negra"
+		},
+		{
+			name:"volt",
+			cinta:"dorada"
+		},
+		{
+			name:"redF",
+			cinta:"amarilla"
+		}
+	];
 });
